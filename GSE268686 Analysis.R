a8079fe35789af865178db911e8453475bfdf410
Tedngambi <123460861+Tedngambi@users.noreply.github.com>
2026-04-05 23:46:45 +0200

    Update GSE268686 Analysis.R

--- GSE268686 Analysis.R
+++ GSE268686 Analysis.R
@@ -9,6 +9,7 @@ create_github_token()
 gitcreds::gitcreds_set()
 
 use_github()
+use_readme_md()
 
 library(DESeq2)       # Negative Binomial DE model
 library(edgeR)        # CPM normalisation + TMM scaling
